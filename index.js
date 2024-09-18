09551801f22f0b5a559ebeaf39c7479f9c7ab5c4
maiducbinh <binhmai2205@gmail.com>
2024-09-18 07:53:07 +0700

    add admin dashboard

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const routeAdmin = require("./routes/admin/index.route")
 const route = require("./routes/client/index.route")
 const app = express();
 const database = require("./config/database")
@@ -14,6 +15,7 @@ app.use(express.static("public")); // access 'public' folder
 database.connect()
 
 // Routes
+routeAdmin(app);
 route(app);
 
 const port = process.env.PORT;
