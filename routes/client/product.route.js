3ba075a4646acdc0a492e7ef0b3873bfe02b9baa
maiducbinh <binhmai2205@gmail.com>
2024-09-20 21:55:26 +0700

    product layout for admin

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router()
-const controller = require("../../controller/client/product.controller")
+const controller = require("../../controllers/client/product.controller")
 
 router.get("/", controller.index);
 
