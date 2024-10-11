0aeeb71ca4e136d47736a34455767d7d1d3d4a37
maiducbinh <binhmai2205@gmail.com>
2024-10-11 09:26:58 +0700

    produt details for client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,4 @@
+const { status } = require("express/lib/response");
 const Product = require("../../models/product.model")
 
 module.exports.index = async (req, res) => {
@@ -14,3 +15,25 @@ module.exports.index = async (req, res) => {
         products: products
     }); 
 }
+
+module.exports.detail = async (req, res) => {
+    try {
+      const find = {
+        deleted: false,
+        slug: req.params.slug,
+        status: "active"
+      };
+  
+      const product = await Product.findOne(find);
+  
+      console.log(product);
+  
+      res.render("client/pages/products/detail", {
+        pageTitle: product.title,
+        product: product,
+      });
+    } catch (error) {
+      res.redirect("/products");
+    }
+  };
+  
