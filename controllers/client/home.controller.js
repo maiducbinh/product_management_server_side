320016d98112eb4eb7517827e852687eaef84db6
maiducbinh <binhmai2205@gmail.com>
2024-11-03 08:18:34 +0700

    adding menu for client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,6 @@
-module.exports.index = (req, res) => {
-    res.render("client/pages/home/index.pug", {
-        pageTitle: "Trang chủ"
+// [GET]
+module.exports.index = async (req, res) => {
+    res.render("client/pages/home/index", {
+        pageTitle: "Trang chủ",
     });
-}
+};
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,9 @@
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
-const homeRoutes = require("./home.route")
 
 module.exports = (app) => {
+    app.use(categoryMiddleware.category);
     app.use("/", homeRoutes);
-
     app.use("/products", productRoutes);
-}
+};
