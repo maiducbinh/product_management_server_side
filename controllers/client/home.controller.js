0f14135e2536bbad3b1e07209c22073bb4d9f582
maiducbinh <binhmai2205@gmail.com>
2024-11-04 09:12:27 +0700

    adding new products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -10,10 +10,22 @@ module.exports.index = async (req, res) => {
         status: "active",
     });
     // Hết Lấy ra sản phẩm nổi bật
-    const newProducts = productsHelper.priceNewProducts(productsFeatured);
+    const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
 
+    // Hiển thị danh sách sản phẩm mới nhất
+    const productsNew = await Product.find({
+            deleted: false,
+            status: "active",
+        })
+        .sort({
+            position: "desc"
+        })
+        .limit(6);
+    // Hết Hiển thị danh sách sản phẩm mới nhất
+    const newProductsNew = productsHelper.priceNewProducts(productsNew);
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
-        productsFeatured: newProducts,
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew,
     });
 };
