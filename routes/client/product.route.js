4c9f1d8e6756322d470023688d402314b2d441a4
maiducbinh <binhmai2205@gmail.com>
2024-11-05 09:15:02 +0700

    updating product detail

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -4,6 +4,6 @@ const controller = require("../../controllers/client/product.controller")
 
 router.get("/", controller.index);
 router.get("/:slugCategory", controller.category);
-// router.get("/:slug", controller.detail);
+router.get("/detail/:slugProduct", controller.detail);
 
 module.exports = router;
